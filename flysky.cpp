1147c14fd684c1f7c0fa233d2ce6d99dc61a5163
jordojordo <jordonleach@gmail.com>
2022-09-25 10:57:12 -0400

    f

--- flysky.cpp
+++ flysky.cpp
@@ -9,8 +9,11 @@ static IBusBM ibus;
 int readChannel(byte channelInput, int minLimit, int maxLimit,
                 int defaultValue) {
   uint16_t ch = ibus.readChannel(channelInput);
-  if (ch < 100)
+  
+  if (ch < 100) {
     return defaultValue;
+  }
+
   return map(ch, 1000, 2000, minLimit, maxLimit);
 }
 
--- lawndon.ino
+++ lawndon.ino
@@ -9,18 +9,12 @@
 static IBusBM ibus;
 
 void setup() {
-  // Start serial monitor
   Serial.begin(115200);
 
-  // Attach iBus object to serial port
   ibus.begin(Serial);
 }
 
 void loop() {
-
-  // Cycle through first 5 channels and determine values
-  // Print values to serial monitor
-  // Note IBusBM library labels channels starting with "0"
   for (byte i = 0; i < 5; i++) {
     int value = readChannel(i, -100, 100, 0);
     Serial.print("Ch");
